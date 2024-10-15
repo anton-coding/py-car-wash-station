1c93348c7f1cc599e5539a46e301a8126bf44a0c
Anton Savka <anton.savka.business@gmail.com>
2024-10-15 21:22:49 +0300

    Provided the solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 class Car:
     def __init__(
             self, comfort_class: int,
-            clean_mark: int,
-            brand: str) -> None:
+            clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -22,8 +21,9 @@ class CarWashStation:
 
     def serve_cars(self, cars: list) -> float:
         """
-        The method, that takes a list of cars, washes only cars with clean_mark < clean_power of
-        wash station and returns income of CarWashStation for serving this list of cars,
+        The method, that takes a list of cars, washes only cars with
+        clean_mark < clean_power of  wash station and returns income of
+        CarWashStation for serving this list of cars,
         rounded to 1 decimal.
         """
         total_income = 0.0
@@ -35,9 +35,11 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         """
-        The method, that calculates cost for a single car wash, cost is calculated as: car's comfort class
-        * difference between wash station's clean power and car's clean mark
-        * car wash station rating / car wash station distance to the center of the city.
+        The method, that calculates cost for a single car wash,
+        cost is calculated as: car's comfort class
+        * difference between wash station's clean power and
+        car's clean mark * car wash station rating / car wash station
+        distance to the center of the city.
         """
         comfort_factor = car.comfort_class
         clean_difference = self.clean_power - car.clean_mark
@@ -48,7 +50,8 @@ class CarWashStation:
 
     def wash_single_car(self, car: Car) -> int:
         """
-        The method, that washes a single car, so it should have clean_mark equals wash station's clean_power,
+        The method, that washes a single car, so it should have clean_mark
+        equals wash station's clean_power,
         if wash_station.clean_power is greater than car.clean_mark.
         """
         car.clean_mark = self.clean_power
@@ -56,9 +59,11 @@ class CarWashStation:
     def rate_service(self, new_rating: int | float) -> None:
         """
         The method that adds a single rate to the wash station,
-        and based on this single rate average_rating and count_of_ratings should be changed.
+        and based on this single rate average_rating and
+        count_of_ratings should be changed.
         """
         total_rating = (self.average_rating * self.count_of_ratings)
         total_rating += new_rating
         self.count_of_ratings += 1
-        self.average_rating = round(total_rating / self.count_of_ratings, 1)
+        self.average_rating = round(total_rating
+                                    / self.count_of_ratings, 1)
